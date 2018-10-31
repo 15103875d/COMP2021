9f95ec10a8162ed5b16130e0bf00ba4d5be1aaef
WilliamStokke <44631160+WilliamStokke@users.noreply.github.com>
2018-10-31 16:49:18 +0800

    Update piece.java

--- comp2021/src/jungle/piece.java
+++ comp2021/src/jungle/piece.java
@@ -1,4 +1,21 @@
 package jungle;
 
 public class piece {
+  int rank; 
+  char positionX; 
+  int positionY; 
+  boolean team; 
+  boolean isRemoved; 
+  
+  //constructor piece 
+  public piece(int rank, char positionX, int positionY, boolean team, boolean isRemoved){
+    this.rank = rank; 
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.team = team;
+    this.isRemoved = isRemoved; 
+    
+  }
+  
+  
 }
